ae96b5b4e4b642bc85a592d705449d983eecc108
Anthony <anthonychytros@gmail.com>
2026-08-10 09:32:36 -0400

    update to v0.11rc1

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ language = "en"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "0.10.20260723"
+release = "0.11rc1"
 
 # -- General configuration ---------------------------------------------------
 
